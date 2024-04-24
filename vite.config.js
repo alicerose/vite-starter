dc79b80454204494a39d3d2484da7e648768cd30
Alice Rose <czk@seeds-9.com>
2024-04-24 14:58:14 +0900

    create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, 'src')
+
+export default defineConfig({
+    root: 'src',
+
+    build: {},
+
+    plugins: []
+})
