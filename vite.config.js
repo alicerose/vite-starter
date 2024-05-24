db2f097170fca7f7b537975422adc1367183d59f
Alice Rose <czk@seeds-9.com>
2024-05-24 19:55:57 +0900

    apply biome

--- vite.config.js
+++ vite.config.js
@@ -1,64 +1,66 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { ViteEjsPlugin } from "vite-plugin-ejs";
-import * as path from "path";
-import {globSync} from "glob";
-import autoprefixer from "autoprefixer";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import * as path from 'node:path';
+import { globSync } from 'glob';
+import autoprefixer from 'autoprefixer';
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'src');
 
-const htmlFiles = globSync('src/**/*.html')
-    .reduce((acc, curr) => {
-        const key = path.relative('src', curr).replace('.html', '')
-        acc[key] = curr
-        return acc
-    }, {});
+const htmlFiles = globSync('src/**/*.html').reduce((acc, curr) => {
+	const key = path.relative('src', curr).replace('.html', '');
+	acc[key] = curr;
+	return acc;
+}, {});
 
 export default defineConfig({
-    root: root,
-    publicDir: '../public',
+	root: root,
+	publicDir: '../public',
 
-    css: {
-        postcss: {
-            plugins: [autoprefixer]
-        }
-    },
+	css: {
+		postcss: {
+			plugins: [autoprefixer],
+		},
+	},
 
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true,
 
-        rollupOptions: {
-            input: htmlFiles
-        }
-    },
+		rollupOptions: {
+			input: htmlFiles,
+		},
+	},
 
-    plugins: [
-        ViteEjsPlugin({
-            config: {
-                root: {
-                    path: {
-                        src: './',
-                        ejs: './ejs',
-                    }
-                },
-                sub: {
-                    path: {
-                        src: '../',
-                        ejs: './ejs',
-                    }
-                }
-            }
-        }, {
-            ejs: {
-                beautify: true,
-            }
-        }),
-    ],
+	plugins: [
+		ViteEjsPlugin(
+			{
+				config: {
+					root: {
+						path: {
+							src: './',
+							ejs: './ejs',
+						},
+					},
+					sub: {
+						path: {
+							src: '../',
+							ejs: './ejs',
+						},
+					},
+				},
+			},
+			{
+				ejs: {
+					beautify: true,
+				},
+			},
+		),
+	],
 
-    resolve: {
-        alias: {
-            '@/' : `${root}/`
-        }
-    }
-})
+	resolve: {
+		alias: {
+			'@/': `${root}/`,
+		},
+	},
+});
