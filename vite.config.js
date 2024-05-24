5919d64f3b85defdd4fef57717e417d1bd3fb492
Alice Rose <czk@seeds-9.com>
2024-05-24 13:34:09 +0900

    enable multiple input htmls

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import * as path from "path";
+import {globSync} from "glob";
 
 const root = resolve(__dirname, 'src')
+
+const htmlFiles = globSync('src/**/*.html')
+    .reduce((acc, curr) => {
+        const key = path.relative('src', curr).replace('.html', '')
+        acc[key] = curr
+        return acc
+    }, {});
 
 export default defineConfig({
     root: root,
@@ -11,6 +19,10 @@ export default defineConfig({
 
     build: {
         outDir: '../dist',
+
+        rollupOptions: {
+            input: htmlFiles
+        }
     },
 
     plugins: [
