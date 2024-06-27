60209b438b3b12817826e029abc16b67304e1b03
Alice Rose <czk@seeds-9.com>
2024-06-27 16:57:27 +0900

    modify data structure

--- data/default.js
+++ data/default.js
@@ -1,6 +1,22 @@
 export default {
-	url: 'https://example.com',
-	gtm: 'GTM-XXXXYYYY',
-	title: 'default title',
-	description: 'default description',
+	base: {
+		url: 'https://example.com',
+		title: 'Website title placeholder.',
+		description: 'Website common description placeholder.',
+		gtm: '',
+	},
+	routes: {},
+	pages: {
+		index: {
+			title: null,
+			description: null,
+			path: '/',
+			meta: ['<link rel="canonical" href="https://example.com/hoge" />'],
+		},
+		sub: {
+			title: 'Sub Page',
+			description: 'Sub Description',
+			path: '/sub/',
+		},
+	},
 };
